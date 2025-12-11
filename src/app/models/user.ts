61ace145f6f4a21c6b0e42c80746c68b47f6d007
MuhammadMuazAhmd <muhammadmuazahmed@gmail.com>
2025-12-11 14:22:01 +0500

    userSchema added

---
+++ src/app/models/user.ts
@@ -0,0 +1,73 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+
+export interface Message extends Document {
+    content: string;
+    createdAt: Date;
+}
+
+
+
+const messageSchema: Schema<Message> = new Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        requred: true,
+        default: Date.now
+    }
+})
+
+
+export interface User extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    verificationCode: string;
+    verificationCodeExpiry: Date;
+    isReceivngMessages: boolean;
+    messages: Message[];
+}
+
+const userSchema: Schema<User> = new Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, "Please enter a valid email address"]
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now
+    },
+    verificationCode: {
+        type: String,
+        required: true
+    },
+    verificationCodeExpiry: {
+        type: Date,
+        required: true
+    },
+    isReceivngMessages: {
+        type: Boolean,
+        required: true,
+        default: false
+    },
+    messages: [messageSchema]
+})
+
+export default mongoose.model<User>("User", userSchema);
