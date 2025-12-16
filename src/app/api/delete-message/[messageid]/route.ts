f2615e4e0d1a8bdc44fc5b1d3da2c29b63674d04
MuhammadMuazAhmd <muhammadmuazahmed@gmail.com>
2025-12-16 19:29:38 +0500

    Delete-message api integrated

--- src/app/api/delete-message/[messageid]/route.ts
+++ src/app/api/delete-message/[messageid]/route.ts
@@ -4,14 +4,21 @@ import { getServerSession } from "next-auth";
 import { options } from "../../auth/[...nextauth]/options";
 
 export async function DELETE(request: Request, { params }: { params: { messageid: string } }) {
+    const { messageid } = params;
+    await dbconnect();
+    const session = await getServerSession(options);
+    if (!session || !session.user) {
+        return Response.json({ message: "Unauthorized" }, { status: 401 });
+    }
     try {
-        await dbconnect();
-        const session = await getServerSession(options);
-        if (!session) {
-            return Response.json({ message: "Unauthorized" }, { status: 401 });
+        const deletedMessage = await usermodel.updateOne(
+            { _id: session.user.id },
+            { $pull: { messages: { _id: messageid } } }
+        );
+        if (deletedMessage.modifiedCount === 0) {
+            return Response.json({ message: "Message not found" }, { status: 404 });
         }
-        const { messageid } = request.query;
-
+        return Response.json({ message: "Message deleted successfully" }, { status: 200 });
     } catch (error) {
         console.error(error);
         return Response.json({ message: "Internal server error" }, { status: 500 });
