bb034b6ec09b125e9cc8c931000893b720d7a463
MuhammadMuazAhmd <muhammadmuazahmed@gmail.com>
2025-12-18 12:40:19 +0500

    send message api updated

--- src/app/api/send-message/route.ts
+++ src/app/api/send-message/route.ts
@@ -10,7 +10,7 @@ export async function POST(request: Request) {
         if (!user) {
             return Response.json({ message: "User not found" }, { status: 404 });
         }
-        if (!user.isReceivngMessages) {
+        if (!user.isReceivingMessages) {
             return Response.json({ message: "User is not receiving messages" }, { status: 400 });
         }
         const newmessage = ({
@@ -20,6 +20,29 @@ export async function POST(request: Request) {
         user.messages.push(newmessage as Message);
         await user.save();
         return Response.json({ message: "Message sent successfully" }, { status: 200 });
+    }
+    catch (error) {
+        console.error(error);
+        return Response.json({ message: "Internal server error" }, { status: 500 });
+    }
+}
+
+export async function GET(request: Request) {
+    await dbconnect();
+    const { searchParams } = new URL(request.url)
+    const username = searchParams.get("username")
+    try {
+        const user = await usermodel.findOne({ username });
+        if (!user) {
+            return Response.json({ message: "User not found" }, { status: 404 });
+        }
+        if (!user.isVerified) {
+            return Response.json({ message: "User is not verified" }, { status: 400 });
+        }
+        if (!user.isReceivingMessages) {
+            return Response.json({ message: "User is not receiving messages" }, { status: 400 });
+        }
+        return Response.json({ messages: user.username }, { status: 200 });
     }
     catch (error) {
         console.error(error);
